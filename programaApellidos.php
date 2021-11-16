33ce38bb0f0f35dbb91caaedf1412c12fc21426c
EmanuelHeredia3063 <jorge.heredia@est.fi.uncoma.edu.ar>
2021-11-16 08:54:03 -0300

    agregue funcion cargarJuegos

--- programaApellidos.php
+++ programaApellidos.php
@@ -6,7 +6,8 @@
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* ... COMPLETAR ... */
-/*Flores Montes Ailen Agustina - FAI-3547 -Tecnicatura Desarrollo Web - ailen.flores@est.fi.uncoma.ed.ar - agusflores1*/
+/*Flores Montes Ailen Agustina - FAI-3547 -Tecnicatura Desarrollo Web - ailen.flores@est.fi.uncoma.ed.ar - agusflores1 */
+/*Jorge Emanuel Heredia - FAI 3063 - Tecnicatura Desarrollo Web - jorge.heredia@est.fi.edu.ar - EmanuelHeredia3063*/
 
 
 
@@ -68,13 +69,13 @@ function solicitarNumeroEntre($min, $max)
 function seleccionarOpcion ()
 {
 echo "Menu de opciones:
-\n 1)Jugar al tateti
-\n 2)Mostrar un juego
-\n 3) Mostrar el primer juego ganador
-\n4)Mostrar el primer juego ganador
-\n5)Mostrar resumen del jugador
-\n6) Mostrar listado de juegos ordenados por jugador O
-\n7)Salir \n";
+    1) Jugar al tateti
+    2) Mostrar un juego
+    3) Mostrar el primer juego ganador
+    4) Mostrar el primer juego ganador
+    5) Mostrar resumen del jugador
+    6) Mostrar listado de juegos ordenados por jugador O
+    7) Salir \n";
 $opcion = trim(fgets(STDIN)) ;
 if ($opcion>7)
     {
@@ -82,6 +83,57 @@ if ($opcion>7)
     } return $opcion ;
 }
 
+/*Funcion que precarga 10 juegos al iniciar el programa 
+@return array 
+*/
+
+function cargarJuegos () 
+{
+$arregloColeccionDeJuegos = [];
+$arregloColeccionDeJuegos [0] = [ "jugadorCruz" => "EMA",
+                                  "jugadorCirculo" => "BAUTY",
+                                  "puntosCruz" => 0 ,
+                                  "puntosCirculo" => 6 ];  
+$arregloColeccionDeJuegos [1] = [ "jugadorCruz" => "JULY",
+                                  "jugadorCirculo" => "AGUS",
+                                  "puntosCruz" => 1 ,
+                                  "puntosCirculo" => 1 ];
+$arregloColeccionDeJuegos [2] = [ "jugadorCruz" => "DANI",
+                                  "jugadorCirculo" => "CECI",
+                                  "puntosCruz" => 3 ,
+                                  "puntosCirculo" => 0 ];  
+$arregloColeccionDeJuegos [3] = [ "jugadorCruz" => "GABY",
+                                  "jugadorCirculo" => "EMA",
+                                  "puntosCruz" => 4,
+                                  "puntosCirculo" => 0 ]; 
+$arregloColeccionDeJuegos [4] = [ "jugadorCruz" => "JULY",
+                                  "jugadorCirculo" => "DANI",
+                                  "puntosCruz" => 1 ,
+                                  "puntosCirculo" => 1 ]; 
+$arregloColeccionDeJuegos [5] = [ "jugadorCruz" => "DANI",
+                                  "jugadorCirculo" => "JULY",
+                                  "puntosCruz" => 4 ,
+                                  "puntosCirculo" => 0 ]; 
+$arregloColeccionDeJuegos [6] = [ "jugadorCruz" => "LUCE",
+                                  "jugadorCirculo" => "GABY",
+                                  "puntosCruz" => 3 ,
+                                  "puntosCirculo" => 0 ]; 
+$arregloColeccionDeJuegos [7] = [ "jugadorCruz" => "BAUTY",
+                                  "jugadorCirculo" => "AGUS",
+                                  "puntosCruz" => 0 ,
+                                  "puntosCirculo" => 2 ]; 
+$arregloColeccionDeJuegos [8] = [ "jugadorCruz" => "CECI",
+                                  "jugadorCirculo" => "SUSANA",
+                                  "puntosCruz" => 1,
+                                  "puntosCirculo" => 1 ]; 
+$arregloColeccionDeJuegos [9] = [ "jugadorCruz" => "GERMAN",
+                                  "jugadorCirculo" => "MIGUEL",
+                                  "puntosCruz" => 0 ,
+                                  "puntosCirculo" => 4 ];                                 
+
+return $arregloColeccionDeJuegos ;  
+
+}
 
 
 
@@ -97,6 +149,12 @@ if ($opcion>7)
 
 
 //Proceso:
+
+//PARTE EMANUEL
+cargarJuegos() ;
+print_r($arregloColeccionDeJuegos) ;
+
+
 
 //PARTE AGUS
  //INICIAMOS PROGRAMA
